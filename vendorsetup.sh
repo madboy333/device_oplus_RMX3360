067633a46f1252261c906000fcd84e2c94d1a638
itsXRP <itsxrproms@gmail.com>
2022-11-17 15:40:22 +0000

    RMX3360:Update vendorsetup.sh
    Signed-off-by: itsXRP <itsxrproms@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,6 @@
+#Clone Repos
+git clone https://github.com/itsxrp/device_oplus_sm8350-common.git -b elixir device/oplus/sm8350-common && git clone https://github.com/itsxrp/hardware_oplus.git -b elixir hardware/oplus && git clone https://gitlab.com/itsxrp/proprietary_vendor_oplus.git -b elixir vendor/oplus && git clone https://github.com/pjgowtham/android_kernel_oplus_sm8350.git kernel/oplus/sm8350
+
 #Clone vibrator HAL
 rm -rf vendor/qcom/opensource/vibrator
 git clone https://github.com/itsxrp/android_vendor_qcom_opensource_vibrator.git vendor/qcom/opensource/vibrator
@@ -5,6 +8,3 @@ git clone https://github.com/itsxrp/android_vendor_qcom_opensource_vibrator.git 
 #Clone Bluetooth Module
 rm -rf packages/modules/Bluetooth
 git clone https://github.com/itsxrp/android_packages_modules_Bluetooth.git packages/modules/Bluetooth
-
-#Clone ih8sn
-git clone https://github.com/itsxrp/ih8sn.git -b master vendor/ih8sn
